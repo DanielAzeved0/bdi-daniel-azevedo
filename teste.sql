6b84f97d185c6d0936bb6591b816255296926a12
Daniel da Silva Azevedo <Daniel.azevedo081205@gmail.com>
2024-10-29 22:10:54 -0300

    MySQL
    meu primeiro contato com MySQL

---
+++ teste.sql
@@ -0,0 +1,10 @@
+use teste;
+create table cliente 
+(
+	codigo_cliente int,
+    nome_do_cliente varchar(50),
+    sobrenome varchar(50),
+    CPF varchar(50),
+    CEP varchar(50),
+    telefone int
+)
